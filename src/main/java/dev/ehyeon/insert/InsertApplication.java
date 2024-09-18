27e5aa6dbf30c6b68f5f46f50b4b52c7f0ec34db
49EHyeon42 <49ehyeon42@gmail.com>
2024-09-18 17:58:46 +0900

    init: project

---
+++ src/main/java/dev/ehyeon/insert/InsertApplication.java
@@ -0,0 +1,12 @@
+package dev.ehyeon.insert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsertApplication.class, args);
+	}
+}
