e74dd9854bc9d96cf6cb40856212e87ef5309f65
49EHyeon42 <49ehyeon42@gmail.com>
2024-09-19 14:57:57 +0900

    feat: JDBC insert/batch insert, JPA save/saveAll을 테스트할 수 있도록 클래스 추가

---
+++ src/main/java/dev/ehyeon/insert/post/dto/InsertPostDto.java
@@ -0,0 +1,4 @@
+package dev.ehyeon.insert.post.dto;
+
+public record InsertPostDto(String title, String content) {
+}
---
+++ src/main/java/dev/ehyeon/insert/post/repository/PostJdbcDao.java
@@ -0,0 +1,49 @@
+package dev.ehyeon.insert.post.repository;
+
+import dev.ehyeon.insert.post.dto.InsertPostDto;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class PostJdbcDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public void insert(String title, String content) {
+        String sql = "INSERT INTO post (title, content, created_date, last_modified_date) VALUES (?, ?, ?, ?)";
+
+        LocalDateTime now = LocalDateTime.now();
+
+        jdbcTemplate.update(sql, title, content, now, now);
+    }
+
+    public void batchInsert(List<InsertPostDto> insertPostDtos) {
+        String sql = "INSERT INTO post (title, content, created_date, last_modified_date) VALUES (?, ?, ?, ?)";
+
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                InsertPostDto insertPostDto = insertPostDtos.get(i);
+
+                LocalDateTime now = LocalDateTime.now();
+
+                ps.setString(1, insertPostDto.title());
+                ps.setString(2, insertPostDto.content());
+                ps.setObject(3, now);
+                ps.setObject(4, now);
+            }
+
+            @Override
+            public int getBatchSize() {
+                return insertPostDtos.size();
+            }
+        });
+    }
+}
